4163811c135162083ea79ee74cb6f4f14939a111
Mubtasim Ariyan <ariyanmubtasim@gmail.com>
2025-04-05 02:19:48 +0600

    Delete Lab 8 directory

--- Lab 8/Circle.java
+++
@@ -1,8 +0,0 @@
-public class Circle extends Shape {
-    public double radius;
-
-    public void area() {
-        double area = Math.PI * radius * radius;
-        System.out.println("Area of " + color + " " + name + ": " + area);
-    }
-}
